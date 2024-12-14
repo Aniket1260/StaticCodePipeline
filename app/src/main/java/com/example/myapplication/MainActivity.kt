fc751a3434eb0db0650170799948048a1dc02619
aniket1260 <anket1260@gmail.com>
2024-12-14 12:43:55 +0530

    Make gradlew executable

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -45,3 +45,10 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+fun badFormatting() { println( "Hello World!" ) }
+fun longFunction() {
+    val a = 1
+    val b = 2
+    val c = 3
+    println(a + b + c)
+}
